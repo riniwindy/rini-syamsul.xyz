e75b09f1354a0525c93ceda28eb6229ab9242837
riniwindy <riniwindyastuti@yahoo.com>
2017-01-05 10:34:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 	$('#counter').countdown({
 				
-		timestamp : (new Date()).getTime() + 11*24*60*60*1000
+		timestamp : (new Date()).getTime() + new Date("Maret 04, 2017 07:30:00").getTime()
 	});
 	
 	$('#tes')({
